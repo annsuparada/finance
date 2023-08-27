ae1a75a25f8e2ac8661abc07650475d49f19a568
Ann <ann.suparada@gmail.com>
2023-08-27 14:17:35 -0700

    create links for social media icons

--- client/src/components/SocialMediaLinks.tsx
+++ client/src/components/SocialMediaLinks.tsx
@@ -3,14 +3,28 @@ import FacebookIcon from '@mui/icons-material/Facebook'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import YouTubeIcon from '@mui/icons-material/YouTube'
+import { Link } from '@mui/material'
 
 const SocialMediaLinks: React.FC = () => {
+  const styles = {
+    icon: {
+      padding: '2px',
+    },
+  }
   return (
     <>
-      <FacebookIcon color="secondary" />
-      <InstagramIcon color="secondary" />
-      <TwitterIcon color="secondary" />
-      <YouTubeIcon color="secondary" />
+      <Link href="#" rel="noopener noreferrer">
+        <FacebookIcon color="secondary" style={styles.icon} />
+      </Link>
+      <Link href="#" target="_blank" rel="noopener noreferrer">
+        <InstagramIcon color="secondary" style={styles.icon} />
+      </Link>
+      <Link href="#" target="_blank" rel="noopener noreferrer">
+        <TwitterIcon color="secondary" style={styles.icon} />
+      </Link>
+      <Link href="#" target="_blank" rel="noopener noreferrer">
+        <YouTubeIcon color="secondary" style={styles.icon} />
+      </Link>
     </>
   )
 }
