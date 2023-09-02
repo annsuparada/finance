5b1ece792aff5da627352a4d9044994171b2124e
Ann <ann.suparada@gmail.com>
2023-09-02 15:29:22 -0700

    add loading component to article list

--- client/src/components/Organism/ArticleList.tsx
+++ client/src/components/Organism/ArticleList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { primary } from '../../theme'
+import Loading from '../atom/Loading'
 import BigCard from '../molecular/BigCard'
 import SmallCard from '../molecular/SmallCard'
 
@@ -33,20 +34,32 @@ const Articles: React.FC<ArticlesProps> = ({ articles, loading }) => {
   }
 
   return (
-    <div>
-      <div style={styles.divider}>
-        <h1 style={styles.heading}>Investing</h1>
-      </div>
-
+    <>
       {loading ? (
-        <p>Loading...</p>
+        <Loading />
       ) : (
-        <div style={styles.grid}>
-          {articles.map((item, index) => (
-            <>
-              {index === 1 ? (
-                <div style={styles.item1}>
-                  <BigCard
+        <div>
+          <div style={styles.divider}>
+            <h1 style={styles.heading}>Investing</h1>
+          </div>
+
+          <div style={styles.grid}>
+            {articles.map((item, index) => (
+              <>
+                {index === 1 ? (
+                  <div style={styles.item1}>
+                    <BigCard
+                      key={item.id}
+                      id={item.id}
+                      alt={item.alt}
+                      title={item.title}
+                      imageURL={item.imageURL}
+                      articleContent={item.articleContent}
+                      slug={item.slug}
+                    />
+                  </div>
+                ) : (
+                  <SmallCard
                     key={item.id}
                     id={item.id}
                     alt={item.alt}
@@ -55,40 +68,30 @@ const Articles: React.FC<ArticlesProps> = ({ articles, loading }) => {
                     articleContent={item.articleContent}
                     slug={item.slug}
                   />
-                </div>
-              ) : (
-                <SmallCard
-                  key={item.id}
-                  id={item.id}
-                  alt={item.alt}
-                  title={item.title}
-                  imageURL={item.imageURL}
-                  articleContent={item.articleContent}
-                  slug={item.slug}
-                />
-              )}
-            </>
-          ))}
+                )}
+              </>
+            ))}
+          </div>
+
+          <div style={styles.divider}>
+            <h1 style={styles.heading}>Latest</h1>
+          </div>
+          <div style={styles.grid}>
+            {articles.map((item, index) => (
+              <SmallCard
+                key={item.id}
+                id={item.id}
+                alt={item.alt}
+                title={item.title}
+                imageURL={item.imageURL}
+                articleContent={item.articleContent}
+                slug={item.slug}
+              />
+            ))}
+          </div>
         </div>
       )}
-
-      <div style={styles.divider}>
-        <h1 style={styles.heading}>Latest</h1>
-      </div>
-      <div style={styles.grid}>
-        {articles.map((item, index) => (
-          <SmallCard
-            key={item.id}
-            id={item.id}
-            alt={item.alt}
-            title={item.title}
-            imageURL={item.imageURL}
-            articleContent={item.articleContent}
-            slug={item.slug}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   )
 }
 
