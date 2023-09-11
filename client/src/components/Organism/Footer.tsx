de43698a20c67b0dea1963f4ebdb453550af9c0f
Ann <ann.suparada@gmail.com>
2023-09-11 15:50:39 -0700

    update footer

--- client/src/components/Organism/Footer.tsx
+++ client/src/components/Organism/Footer.tsx
@@ -1,22 +1,74 @@
+import { useMediaQuery } from '@mui/material'
 import React from 'react'
-import { primary, secondary } from '../../theme'
+import { Link } from 'react-router-dom'
+import { mobileView, primary, secondary, tabletView } from '../../theme'
+import SocialMediaLinks from '../molecular/SocialMediaLinks'
 
 const Footer: React.FC = () => {
+  const isMobile = useMediaQuery(`(max-width:${mobileView})`)
+  const isTablet = useMediaQuery(`(max-width:${tabletView})`)
+
   const styles = {
     container: {
-      width: '100%',
       marginTop: '60px',
+      padding: '3rem',
       backgroundColor: primary,
       minHeight: '25vh',
       flexShrink: 0,
+      display: 'grid',
+      gridTemplateColumns: isMobile
+        ? '1fr'
+        : isTablet
+        ? '1fr 1fr'
+        : 'repeat(3, 1fr)',
+    },
+    gridItem: {
+      display: 'grid',
+      gridTemplateColumns: '1fr',
+      justifyContent: 'start',
+      alignContent: 'start',
     },
     text: {
       color: secondary,
     },
+    p: {
+      color: secondary,
+      fontSize: '16px',
+      marginTop: '0',
+    },
   }
   return (
     <div style={styles.container}>
-      <h2 style={styles.text}>Footer</h2>
+      <div style={styles.gridItem}>
+        <h3 style={styles.text}>Sections</h3>
+        <Link to="/investing" className="footer-links">
+          Investing
+        </Link>
+        <Link to="/calculator" className="footer-links">
+          Calculator
+        </Link>
+      </div>
+      <div style={styles.gridItem}>
+        <h3 style={styles.text}>About Us</h3>
+        <Link to="/" className="footer-links">
+          About Us
+        </Link>
+        <Link to="/" className="footer-links">
+          Affiliate Disclosure
+        </Link>
+        <div>
+          <SocialMediaLinks />
+        </div>
+      </div>
+      <div style={styles.gridItem}>
+        <h3 style={styles.text}>Disclamer</h3>
+        <p style={styles.p}>
+          The information provided on this website is for educational and
+          informational purposes only. It should not be considered financial or
+          investment advice. Please conduct your own research and consult with a
+          qualified professional before making financial decisions.
+        </p>
+      </div>
     </div>
   )
 }
