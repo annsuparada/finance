41c18c6ad71ae0f4dc00ac637775d898851b2e00
Ann <ann.suparada@gmail.com>
2023-09-11 13:17:39 -0700

    create divider component

---
+++ client/src/components/atom/Divider.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { primary } from '../../theme'
+
+interface DividerProps {
+  text: string
+  fontSize?: string
+  color?: string
+}
+const Divider: React.FC<DividerProps> = ({ text, fontSize, color }) => {
+  const styles = {
+    divider: {
+      width: '100%',
+      borderBottom: `2px solid ${color}`,
+      padding: '5px',
+      margin: '30px 0 25px 0',
+    },
+    heading: {
+      margin: 0,
+      fontSize: fontSize,
+    },
+  }
+  return (
+    <div style={styles.divider}>
+      <h2 style={styles.heading}>{text}</h2>
+    </div>
+  )
+}
+
+Divider.defaultProps = {
+  fontSize: '45px',
+  color: primary,
+}
+
+export default Divider
