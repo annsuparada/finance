2e4f1566e7c92eb9f6abaa1d0dc5dd417a3bfab7
Ann <ann.suparada@gmail.com>
2023-09-14 14:35:09 -0700

    create helpers

---
+++ server/helpers/index.ts
@@ -0,0 +1,16 @@
+import crypto from 'crypto'
+
+const SECRET = process.env.SECRET
+
+export const authentication = (salt: string, password: string): string => {
+  if (SECRET !== undefined) {
+    return crypto
+      .createHmac('sha256', [salt, password].join('/'))
+      .update(SECRET)
+      .digest('hex')
+  } else {
+    throw new Error('SECRET environment variable is undefined')
+  }
+}
+
+export const random = () => crypto.randomBytes(128).toString('base64')
