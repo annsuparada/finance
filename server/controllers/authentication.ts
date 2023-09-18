c6c4c4e6cad791de0bf63eea9106d5b0fe88795d
Ann <ann.suparada@gmail.com>
2023-09-18 16:55:39 -0700

    refactor registor controller

--- server/controllers/authentication.ts
+++ server/controllers/authentication.ts
@@ -11,6 +11,12 @@ interface errorObject {
   message: string
 }
 
+interface registerPostBodyObj {
+  email: string
+  password: string
+  username: string
+}
+
 export const login = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password } = req.body
@@ -57,10 +63,55 @@ export const login = async (req: express.Request, res: express.Response) => {
   }
 }
 
-export const register = async (req: express.Request, res: express.Response) => {
-  const result = await register(req.body, res)
-  if (result.errors) {
-    return res.status(400).json(result.errors)
+export const register = async (postBody: registerPostBodyObj) => {
+  const { email, password, username } = postBody
+  const errorMessages: errorObject[] = []
+
+  if (!email) {
+    errorMessages.push({
+      code: 'requiredEmail',
+      message: 'Email is required',
+    })
+  } else if (!password) {
+    errorMessages.push({
+      code: 'requiredPassword',
+      message: 'Password is required',
+    })
+  } else if (!username) {
+    errorMessages.push({
+      code: 'requiredUsername',
+      message: 'Username is required',
+    })
   }
-  return res.send(result)
+
+  const existingUser = await getUserByEmail(email)
+  const existingUsername = await getUserByUsername(username)
+  if (existingUser) {
+    errorMessages.push({
+      code: 'emailExists',
+      message: 'This email is already in use.',
+    })
+  }
+  if (existingUsername) {
+    errorMessages.push({
+      code: 'usernameExists',
+      message: 'This username is already taken.',
+    })
+  }
+
+  if (errorMessages.length > 0) {
+    return { errors: errorMessages }
+  }
+
+  const salt = random()
+  const user = await createUser({
+    email,
+    username,
+    authentication: {
+      salt,
+      password: authentication(salt, password),
+    },
+  })
+
+  return { user }
 }
--- server/router/authentication.ts
+++ server/router/authentication.ts
@@ -1,7 +1,21 @@
 import express from 'express'
-import { register, login } from '../controllers/authentication'
+import { login, register } from '../controllers/authentication'
+
+const registerRoute = async (req: express.Request, res: express.Response) => {
+  try {
+    const result = await register(req.body)
+    if (result.errors) {
+      return res.status(400).json(result.errors)
+    }
+
+    return res.send(result)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+}
 
 export default (router: express.Router) => {
-  router.post('/auth/register', register)
+  router.post('/auth/register', registerRoute)
   router.post('/auth/login', login)
 }
