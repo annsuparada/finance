6c7b9de198bf36aaee7eecb03bbb9f4370aec1b4
Ann <ann.suparada@gmail.com>
2023-09-18 17:22:58 -0700

    refactor login

--- server/controllers/authentication.ts
+++ server/controllers/authentication.ts
@@ -11,56 +11,68 @@ interface errorObject {
   message: string
 }
 
+interface loginPostBodyObj {
+  email: string
+  password: string
+}
+
 interface registerPostBodyObj {
   email: string
   password: string
   username: string
 }
 
-export const login = async (req: express.Request, res: express.Response) => {
-  try {
-    const { email, password } = req.body
+export const login = async (postBody: loginPostBodyObj) => {
+  const { email, password } = postBody
+  const errorMessages: errorObject[] = []
 
-    if (!email || !password) {
-      return res.sendStatus(400)
-    }
-
-    const user = await getUserByEmail(email).select(
-      '+authentication.salt +authentication.password',
-    )
-
-    if (!user) {
-      return res.sendStatus(400)
-    }
-
-    const expectedHash = authentication(
-      user.authentication?.salt || '',
-      password,
-    )
-
-    if (!user.authentication || user.authentication.password !== expectedHash) {
-      return res.sendStatus(403)
-    }
-
-    const salt = random()
-    user.authentication.sessionToken = authentication(salt, user._id.toString())
-
-    await user.save()
-
-    if (SECRET !== undefined) {
-      res.cookie(SECRET, user.authentication.sessionToken, {
-        domain: 'localhost',
-        path: '/',
-      })
-    } else {
-      console.error('SECRET environment variable is undefined')
-    }
-
-    return res.status(200).json(user).end()
-  } catch (error) {
-    console.log(error)
-    return res.sendStatus(400)
+  if (!email) {
+    errorMessages.push({
+      code: 'requiredEmail',
+      message: 'Email is required',
+    })
+  } else if (!password) {
+    errorMessages.push({
+      code: 'requiredPassword',
+      message: 'Password is required',
+    })
   }
+
+  const user = await getUserByEmail(email).select(
+    '+authentication.salt +authentication.password',
+  )
+
+  if (!user) {
+    errorMessages.push({
+      code: 'emailNotExists',
+      message: 'Email does not exist',
+    })
+  }
+
+  const expectedHash = authentication(
+    user?.authentication?.salt || '',
+    password,
+  )
+
+  if (!user?.authentication || user?.authentication.password !== expectedHash) {
+    errorMessages.push({
+      code: 'InvalidCredentials',
+      message: 'Invalid credentials',
+    })
+  }
+
+  if (errorMessages.length > 0) {
+    return { errors: errorMessages }
+  }
+
+  const salt = random()
+  if (user?.authentication) {
+    user.authentication.sessionToken = authentication(salt, user._id.toString())
+  }
+
+  await user?.save()
+
+  return { user }
 }
 
 export const register = async (postBody: registerPostBodyObj) => {
--- server/router/authentication.ts
+++ server/router/authentication.ts
@@ -1,6 +1,20 @@
 import express from 'express'
 import { login, register } from '../controllers/authentication'
 
+const loginRoute = async (req: express.Request, res: express.Response) => {
+  try {
+    const result = await login(req.body)
+    if (result.errors) {
+      return res.status(400).json(result.errors)
+    }
+
+    return res.send(result)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+}
+
 const registerRoute = async (req: express.Request, res: express.Response) => {
   try {
     const result = await register(req.body)
@@ -17,5 +31,5 @@ const registerRoute = async (req: express.Request, res: express.Response) => {
 
 export default (router: express.Router) => {
   router.post('/auth/register', registerRoute)
-  router.post('/auth/login', login)
+  router.post('/auth/login', loginRoute)
 }
